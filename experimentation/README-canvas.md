089b4869e8c315580a40ba5c500afd20b9ead1d4
beesanderson <87204074+beesanderson@users.noreply.github.com>
2021-11-18 22:33:38 -0600

    EOD 11/18/21

---
+++ experimentation/README-canvas.md
@@ -0,0 +1,3 @@
+# Most important of notes: 
+
+Consider, no not consider *MUST* use `drawImage()` to include "Rick Rolled" Easter egg because I'm still living in 2010 when this was still funny. 
---
+++ experimentation/script-canvas.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+// ctx.strokeStyle = '#19bd39';
+ctx.fillStyle = '#19bd39';
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
