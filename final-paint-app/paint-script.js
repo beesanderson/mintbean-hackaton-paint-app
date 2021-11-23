818d52ee52a868fd3ab732f7b4aefc9739c7275c
beesanderson <87204074+beesanderson@users.noreply.github.com>
2021-11-23 17:42:06 -0600

    EOD 11/23/21 Pick up Baby Time

--- final-paint-app/paint-script.js
+++ final-paint-app/paint-script.js
@@ -8,3 +8,50 @@ toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
 
 })
+
+
+
+/* Canvas DOM Variables */
+const canvas = document.querySelector("#canvas")
+const ctx = canvas.getContext("2d")
+
+
+/* Functionality Variables */
+let isDrawing = false
+let lastX = 0
+let lastY = 0
+let hue = 0
+let direction = true
+
+
+/* Base Stroke&Pen Settings */
+ctx.strokeStyle = '#00000'
+ctx.lineJoin = 'round'
+ctx.lineCap = 'round'
+//To change line thickness: 
+ctx.lineWidth = 100;
+
+
+
+/* Functions */
+
+function draw(e) {
+
+}
+
+
+
+
+
+
+
+/* Event Listeners */
+
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true
+    [lastX, lastY] = [e.offsetX, e.offsetY]
+})
+
+canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mouseup', () => isDrawing = false)
+canvas.addEventListener('mouseout', () => isDrawing = false)
