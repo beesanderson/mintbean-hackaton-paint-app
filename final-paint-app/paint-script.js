522684dc9ff3322403cd89a194c74f1d70b6d6ec
beesanderson <87204074+beesanderson@users.noreply.github.com>
2021-11-24 16:41:25 -0600

    Mousey WIP

--- final-paint-app/paint-script.js
+++ final-paint-app/paint-script.js
@@ -10,10 +10,14 @@ toggleButton.addEventListener("click", () => {
 })
 
 
+/* ------------- Variables -------------  */
+
 
 /* Canvas DOM Variables */
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext("2d")
+
+let canvasBounds = canvas.getBoundingClientRect()
 
 
 
@@ -24,13 +28,6 @@ canvas.height = 600;
     //Sets display size using style width and height
     canvas.style.width = "100%"
     canvas.style.height = "100%"
-    //To get position the position of the canvas
-    const canvasBounds = canvas.getBoundingClientRect()
-    // //Set and Create Canvas Bounds (but same as above? hmm)
-    // const bounds = canvas.getBoundingClientRect()
-
-    console.log(canvasBounds)
-
 
 
 /* Mouse Variables */
@@ -41,92 +38,105 @@ const mouse = {
     lastX: 0, lastY: 0,                     // Last frames mouse position
 }
 
-console.log(mouse)
+
 
 
 /* Functionality Variables */
 let isDrawing = false
-let hue = 0
-let direction = true
 
 
 /* Base Stroke&Pen Settings */
 ctx.strokeStyle = '#000000'
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
-//To change line thickness: 
-// ctx.lineWidth = 100;
 
 
 
-/* Functions */
 
+/* ------------- TESTING AND DEBUGGING -------------  */
 
-function mouseEvent(event) {
-    const bounds = canvas.getBoundingClientRect()
+/* TESTING DEBUGGING Variables */
+let testDisplayX = document.getElementById("x-value")
+let testDisplayY = document.getElementById("y-value")
+let testDisplayCoord = document.getElementById("combo-reg-expression")
+let testMouseX = document.getElementById("mouseX")
+let testMouseY = document.getElementById("mouseY")
+let testMouseMovement = document.getElementById("mouse-position")
+let testDisplayMousePosition = document.getElementById("combo-reg-expression")
 
-    //StackOF post says to get the mouse coordinates you subtract the canvas top left and any scrolling
-    mouse.x = event.pageX = bounds.left - scrollX
-    mouse.y = event.pageY = bounds.top - scrollY
-
-    //Now to properly scale mouse coordinates to the canvas resolution coordinates we have to normalize the mouse coordinates from 0 to 1 then divide the bounds (canvas position) width and height 
-    // TOP LEFT = (0,0)
-    // BOTTOM RIGHT (also includes off canvas) = (1, 1)
-
-    mouse.x /= bounds.width
-    mouse.y /= bounds.height
-
-    //now we scale this to the canvas coordinates by multiplying the normalized coordinates with the canvas resolution
-    mouse.x *= canvas.width
-    mouse.y *= canvas.height
-
-} 
+// testDisplayX.textContent = e.x
+// testDisplayY.textContent = e.y
 
 
 
-function draw(e) {
-    if(!isDrawing) return;
-    console.log(e)
+/* ------------- Functions -------------  */
 
-    //try to call the newly created mouseEvent() function. I don't know the difference between (e) and (events) though I don't know if that will create issues?
-
-    //start the path being down by accessing the context from canvas 
-    ctx.beginPath()
-
-    //moving the line from lastX to lastY
-    ctx.moveTo(lastX, lastY)
-
-    //setting/linking the lastX && lastY to the key(e) evens w e.offsetX && e.offsetY
-    ctx.lineTo(e.offsetX, e.offsetY)
-
-    //to actually draw
-    ctx.stroke()
-
-    //set lastX and lastY from correct coordinates 
-    lastX = e.offsetX
-    lastY = e.offsetY
+// function getCoord(e) {
+function getCoord(e) {
+    let x = e.clientX
+    let y = e.clientY
+    const coords = ` x: ${ e.x } | y: ${ e.y } `
+    testDisplayCoord.textContent = coords
+    console.log(coords)
+    testDisplayX.textContent = e.x
+    testDisplayY.textContent = e.y
 }
 
+function getMousePosition(canvas, e) {
+    canvasBounds = canvas.getBoundingClientRect()
+    return {
+        y: e.clientX - canvasBounds.left,
+        x: e.clientY - canvasBounds.top
+    }
+    
+}
+
+// function getMousePosition() {
+//     let canvasBounds = canvas.getBoundingClientRect()
+//     return {
+//         x: e.clientX - canvasBounds.left,
+//         y: e.clientY - canvasBounds.top
+//     }
+// }
 
 
+/* ------------- Event Listeners -------------  */
 
+window.addEventListener("mousemove", (e) => {
 
-/* Event Listeners */
+})
 
-canvas.addEventListener('mousemove', draw)
+// canvas.addEventListener("mousemove", draw)
+
+canvas.addEventListener("mousemove", function (e) {
+    let mousePosition = getMousePosition(canvas, e)
+    testDisplayMousePosition.textContent = canvasBounds
+    testMouseX.textContent = e.x
+    testMouseY.textContent = e.y
+
+})
 
 canvas.addEventListener("mousedown", () => isDrawing = true)
 
-
 canvas.addEventListener('mouseup', () => isDrawing = false)
+
 canvas.addEventListener('mouseout', () => isDrawing = false)
 
 
-// canvas.addEventListener("mousedown", (e) => {
-//     isDrawing = true
-//     [lastX, lastY] = [e.offsetX, e.offsetY]
-// })
 
-// canvas.addEventListener('mousemove', draw)
-// canvas.addEventListener('mouseup', () => isDrawing = false)
-// canvas.addEventListener('mouseout', () => isDrawing = false)
+
+canvas.addEventListener("click", function (e) {
+    let mousePosition = getMousePosition(canvas, e)
+    testDisplayMousePosition.textContent = canvasBounds
+    console.log(mousePosition)
+
+})
+
+canvas.addEventListener("dblclick", getCoord)
+
+// canvas.addEventListener("mousemove", mousePosition)
+
+// `x: ${ e.x } | y: ${ e.y } `
+
+// testDisplayX.textContent = e.x
+// testDisplayY.textContent = e.y
