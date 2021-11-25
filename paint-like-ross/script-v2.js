b279f3b5e7054243a94fb187d5d0a35c2a4174d9
beesanderson <87204074+beesanderson@users.noreply.github.com>
2021-11-25 00:03:38 -0600

    responsive now

--- paint-like-ross/script-v2.js
+++ paint-like-ross/script-v2.js
@@ -3,4 +3,27 @@
 
 /* Canvas DOM Variables */
 const canvas = document.querySelector("#canvas")
+const canvasParent = document.querySelector("#canvasParent")
 const ctx = canvas.getContext("2d")
+
+/* Make Canvas fit its parent container */
+// fitToContainer(canvas)
+canvas.width = canvasParent.offsetWidth;
+canvas.height = canvasParent.offsetHeight;
+
+/* Canvas Display Variables & Function */
+function fitToContainer(canvas) {
+    // Now I did use the class margin-center (margin: 0 auto) to center this in the existing parent...uh oh hopefully that doesn't cause any issues but should be okay
+    canvas.width = canvasParent.offsetWidth;
+    canvas.height = canvasParent.offsetHeight;
+}
+
+
+/* Functionality Variables */
+let isDrawing = false
+
+
+/* Base Stroke&Pen Settings */
+ctx.strokeStyle = '#000000'
+ctx.lineJoin = 'round'
+ctx.lineCap = 'round'
